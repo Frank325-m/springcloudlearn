d5bf7a03aaed80515a8b2629d3c31da51b1f39a8
Frank325-m <liuyide0325@163.com>
2026-01-12 14:17:29 +0800

    完成gateway-service模块的功能

---
+++ gateway-service/src/main/java/com/learn/gateway/GatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package com.learn.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+        System.out.println("===== 网关服务启动成功！端口：9001 =====");
+    }
+}
